8c8f854deb17a741bee48cb706ec23b18eecfc48
Mahmut Can Aslan <canmahmutaslan@gmail.com>
2026-08-03 23:07:06 +0300

    navbar tasarımı geliştirildi

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,34 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
 function Navbar() {
   return (
     <nav>
-      <h2>ASLAN OTOMOTİV</h2>
+      <div>
+        <h2>ASLAN OTOMOTİV</h2>
+      </div>
+
+      <ul>
+        <li>
+          <NavLink to="/">Ana Sayfa</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/araclar">Araçlar</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/hizmetler">Hizmetler</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/hakkimizda">Hakkımızda</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/iletisim">İletişim</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
